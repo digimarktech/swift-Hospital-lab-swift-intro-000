cb2726d8e36343a98eb75402ac8217f9f508ef3b
Marc Aupont <marc@digimarktech.com>
2017-02-10 12:56:38 -0500

    Completed School Lab with all tests passing

--- Hospital/School.swift
+++ Hospital/School.swift
@@ -2,7 +2,129 @@
 //  School.swift
 //  Hospital
 
+protocol Payable {
+    
+    func wages() -> Double
+    
+}
 
+protocol TimeOff {
+    
+    var vacationDays: Int { get set }
+    
+    func requestForVacation(_ days: Int) -> Bool
+    
+}
+
+protocol Reprimand {
+    
+    func disciplineStudent(_ scale: Punishment) -> String
+    
+    func callParentDeliveringMessage(_ scale: Punishment) -> String
+    
+}
+
+protocol Teach {
+    
+    func teachSubject(_ subject: Subject) -> String
+    
+}
+
+protocol Employee: Payable, TimeOff { }
+
+extension Teach {
+    
+    func teachSubject(_ subject: Subject) -> String {
+        
+        switch subject {
+            
+        case .math:
+            return "Take out your math books please."
+            
+        case .science:
+            return "Time to learn the best subject of all! Science!!"
+            
+        case .english:
+            return "To read or not to read. Everyone take out your english books."
+            
+        }
+        
+    }
+}
+
+extension Reprimand {
+    
+    func disciplineStudent(_ scale: Punishment) -> String {
+        
+        switch scale {
+            
+        case .light:
+            return "You're to go back to class, don't do it again."
+            
+        case .medium:
+            return "Why would you think that's a good idea?! You're going to detention."
+            
+        case .severe:
+            return "We are expelling you from the school!"
+            
+        }
+    }
+    
+    func callParentDeliveringMessage(_ scale: Punishment) -> String {
+        
+        switch scale {
+            
+        case .light:
+            return "Your child thought it was funny to put gum in Amy's hair."
+            
+        case .medium:
+            return "Your child has become a terror in the classroom."
+            
+        case .severe:
+            return "Your child has been expelled from the school."
+
+        }
+    }
+    
+}
+
+extension TimeOff {
+    
+    func requestForVacation(_ days: Int) -> Bool {
+        
+        if vacationDays >= days {
+            
+            return true
+            
+        }
+        
+        return false
+    }
+    
+}
+
+extension Payable {
+    
+    func wages()-> Double {
+        
+        return 50_000.00
+    }
+    
+}
+
+enum Punishment {
+    
+    case light
+    case medium
+    case severe
+}
+
+enum Subject {
+    
+    case math
+    case science
+    case english
+}
 
 
 
--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -3,7 +3,42 @@
 //  Hospital
 
 
+struct AdvisoryBoardMember {
+    
+    var name: String
+    var vacationDays: Int = 30
+    
+    func wages() -> Double {
+        
+        return 100_000.00
+    }
+}
 
+struct Principal {
+    
+    var name: String
+    var vacationDays: Int = 20
+    
+    func wages() -> Double {
+        
+        return 80_000.00
+    }
+    
+}
+
+struct Teacher {
+    
+    var name: String
+    var vacationDays: Int = 15
+    
+    
+}
+
+extension Teacher: Employee, Teach { }
+
+extension Principal: Employee, Reprimand { }
+
+extension AdvisoryBoardMember: Employee { }
 
 
 
